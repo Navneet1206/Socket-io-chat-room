78484207a98c657b3a94cc7663253b2ca35b3c93
Navneet Vishwakarma <143477629+Navneet1206@users.noreply.github.com>
2024-09-24 14:27:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,6 @@ def disconnect():
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
 
-# Remove the following block
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable
-#     socketio.run(app, host='0.0.0.0', port=port, debug=True)
+ if __name__ == '__main__':
+     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable
+     socketio.run(app, host='0.0.0.0', port=port, debug=True)
